685d9a76e102be4c38c65fd595ca31321f19487e
Dani Perianto <daniperianto007@gmail.com>
2024-01-06 11:47:28 +0700

    add table and relation (postgreSQL)

---
+++ BE/phpAdmin/app/Models/Player.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Player extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'pgsql';
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'active_avatar',
+        'diamond',
+        'highest_score',
+        'total_score'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function avatar(): BelongsTo
+    {
+        return $this->belongsTo(Avatar::class, 'active_avatar', 'image');
+    }
+
+    public function user_avatar(): HasMany
+    {
+        return $this->hasMany(UserAvatar::class);
+    }
+}
